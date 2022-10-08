04669a2a219d6c036ca7f818ec42ea32708f320f
Sid Kapur <sid@openlabsusa.org>
2022-10-07 22:51:00 -0400

    Add August 2022 data

--- download_data.py
+++ download_data.py
@@ -12,7 +12,7 @@ REGIONS = [
 PREFIX = "https://www2.census.gov/econ/bps/"
 DATA_ROOT = Path("./data")
 
-LATEST_MONTH = (2022, 7)
+LATEST_MONTH = (2022, 8)
 
 # Whether to download the monthly files from December of the latest full year of data available.
 # This is needed in Jan-May of each year, because the full year's estimates (imputing the
